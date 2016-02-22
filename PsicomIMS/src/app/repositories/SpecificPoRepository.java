938f8cd6c2495168cbcb4ffe88ecbcf7c51eb7c1
Abby Estanislao <alyssa.estanislao@yahoo.com>
2016-02-22 19:45:14 +0800

    complete and incomplete order status

--- PsicomIMS/src/app/repositories/SpecificPoRepository.java
+++ PsicomIMS/src/app/repositories/SpecificPoRepository.java
@@ -3,9 +3,14 @@ package app.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import app.entity.Book;
 import app.entity.SpecificPo;
+import app.entity.User;
 
 public interface SpecificPoRepository  extends JpaRepository<SpecificPo, Long> {
+	
+
+	public SpecificPo findByBookId(Book bookId);
 }
 
 
